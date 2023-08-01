00c47678ca644b8ce4c8668f63a4fed0261f0907
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-08-01 12:02:34 +0200

    configure nest passport and jwt in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,35 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // console.log('xxx', configService.get('JWT_SECRET'));
+        // console.log('JWT_SECRET: ', process.env.JWT_SECRET);
+
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '1h' },
+        };
+      },
+    }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: { expiresIn: '1h' },
+    // }),
+  ],
   exports: [TypeOrmModule],
 })
 export class UsersModule {}
